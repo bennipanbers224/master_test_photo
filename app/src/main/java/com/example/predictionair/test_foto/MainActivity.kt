5b34e9fea2d91333338962952f08f853416ad3cc
Gabriel Panjaitan <gabriel@fundtastic.co.id>
2024-08-10 02:21:16 +0700

    camera view android lib

--- app/src/main/java/com/example/predictionair/test_foto/MainActivity.kt
+++ app/src/main/java/com/example/predictionair/test_foto/MainActivity.kt
@@ -1,52 +1,49 @@
 package com.example.predictionair.test_foto
 
 import android.Manifest
-import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
-import android.graphics.Matrix
 import android.os.Bundle
-import android.provider.MediaStore
-import android.util.Base64
 import android.view.View
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.Toast
 import androidx.activity.ComponentActivity
+import androidx.activity.result.ActivityResultCallback
 import androidx.activity.result.contract.ActivityResultContracts
-import io.fotoapparat.Fotoapparat
-import io.fotoapparat.configuration.CameraConfiguration
-import io.fotoapparat.log.fileLogger
-import io.fotoapparat.log.logcat
-import io.fotoapparat.log.loggers
-import io.fotoapparat.parameter.Resolution
-import io.fotoapparat.parameter.ScaleType
-import io.fotoapparat.selector.auto
-import io.fotoapparat.selector.autoFocus
-import io.fotoapparat.selector.back
-import io.fotoapparat.selector.continuousFocusPicture
-import io.fotoapparat.selector.firstAvailable
-import io.fotoapparat.selector.fixed
-import io.fotoapparat.selector.highestFps
-import io.fotoapparat.selector.highestResolution
-import io.fotoapparat.selector.highestSensorSensitivity
-import io.fotoapparat.selector.hz50
-import io.fotoapparat.selector.hz60
-import io.fotoapparat.selector.manualJpegQuality
-import io.fotoapparat.selector.none
-import io.fotoapparat.selector.off
-import io.fotoapparat.view.CameraView
-import java.io.ByteArrayOutputStream
+import androidx.camera.core.AspectRatio
+import androidx.camera.core.Camera
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCapture.OutputFileOptions
+import androidx.camera.core.ImageCapture.OutputFileResults
+import androidx.camera.core.ImageCaptureException
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
+import androidx.core.content.ContextCompat
+import com.google.common.util.concurrent.ListenableFuture
+import java.io.File
+import java.util.concurrent.ExecutionException
+import java.util.concurrent.Executors
+import kotlin.String as String1
+
 
 class MainActivity : ComponentActivity() {
-
-    private lateinit var fotoapparat: Fotoapparat
     lateinit var bitmapResult: Bitmap
-    lateinit var compressKtpBase64:String
+    lateinit var compressKtpBase64: String1
     lateinit var ivCapture:ImageButton
     lateinit var btnNext:Button
     lateinit var btnRepeat:Button
-    lateinit var cameraView:CameraView
+    lateinit var cameraView:PreviewView
+    var cameraFacing:Int = CameraSelector.LENS_FACING_BACK
+
+    private val activityResultLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()
+    ) { result ->
+        if (result) {
+            startCamera(cameraFacing)
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -57,126 +54,100 @@ class MainActivity : ComponentActivity() {
         btnRepeat = findViewById(R.id.btnRepeat)
         cameraView = findViewById(R.id.cameraView)
 
-        chackandOpencamera()
-        cameraInit()
-        setupWatcherListener()
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            activityResultLauncher.launch(Manifest.permission.CAMERA)
+        } else {
+            startCamera(cameraFacing)
+        }
 
     }
 
-    private fun setupWatcherListener(){
-        ivCapture.setOnClickListener {
-            val photoResult = fotoapparat.takePicture()
-            photoResult.toBitmap()
-                .whenAvailable { bitmapPhoto ->
+    fun startCamera(cameraFacing:Int){
+        val aspectRatio = aspectRatio(cameraView.getWidth(), cameraView.getHeight())
+        val listenableFuture: ListenableFuture<ProcessCameraProvider> =
+            ProcessCameraProvider.getInstance(this)
 
-                    if (bitmapPhoto != null) {
-                        val deviceRotate = bitmapPhoto.rotationDegrees
-                        var rotatedBitmap = bitmapPhoto.bitmap
-                        if (deviceRotate == 270) {
-                            rotatedBitmap = rotateBitmap(bitmapPhoto.bitmap, 90f)
-                        }
-                        bitmapResult = rotatedBitmap
-                        fotoapparat.stop()
-                        btnNext.visibility = View.VISIBLE
-                        btnRepeat.visibility = View.VISIBLE
-                        ivCapture.visibility = View.GONE
+        listenableFuture.addListener({
+            try {
+                val cameraProvider: ProcessCameraProvider =
+                    listenableFuture.get() as ProcessCameraProvider
+                val preview: Preview = Preview.Builder().setTargetAspectRatio(aspectRatio).build()
+                val imageCapture = ImageCapture.Builder()
+                    .setCaptureMode(ImageCapture.CAPTURE_MODE_MINIMIZE_LATENCY)
+                    .setTargetRotation(windowManager.defaultDisplay.rotation).build()
+                val cameraSelector = CameraSelector.Builder()
+                    .requireLensFacing(cameraFacing).build()
+                cameraProvider.unbindAll()
+                val camera: Camera =
+                    cameraProvider.bindToLifecycle(this, cameraSelector, preview, imageCapture)
+                ivCapture.setOnClickListener(View.OnClickListener {
+                    if (ContextCompat.checkSelfPermission(
+                            this@MainActivity,
+                            android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+                        ) != PackageManager.PERMISSION_GRANTED
+                    ) {
+                        activityResultLauncher.launch(Manifest.permission.WRITE_EXTERNAL_STORAGE)
                     }
-                }
-        }
-        btnNext.setOnClickListener {
-
-//            val bitmap= BitmapFactory.decodeResource(resources, R.drawable.ktp_example)
-            compressKtpBase64 = compress(bitmapResult)
-        }
-        btnRepeat.setOnClickListener {
-
-            btnNext.visibility = View.GONE
-            btnRepeat.visibility = View.GONE
-            ivCapture.visibility = View.VISIBLE
-            fotoapparat.start()
-        }
-    }
-
-    private fun cameraInit() {
-        fotoapparat= Fotoapparat(
-            context = this,
-            view = cameraView,
-            scaleType = ScaleType.CenterCrop,
-            lensPosition = back(),
-            cameraConfiguration =  CameraConfiguration(
-                pictureResolution = firstAvailable({ Resolution(1280, 720) }, highestResolution()),
-                previewResolution = highestResolution(),
-                previewFpsRange = highestFps(),
-                focusMode = firstAvailable(
-                    continuousFocusPicture(),
-                    autoFocus(),                       // if continuous focus is not available on device, auto focus will be used
-                    fixed()                            // if even auto focus is not available - fixed focus mode will be used
-                ),
-                flashMode = off(),
-                antiBandingMode = firstAvailable(       // (optional) similar to how it is done for focus mode & flash, now for anti banding
-                    auto(),
-                    hz50(),
-                    hz60(),
-                    none()
-                ),
-                jpegQuality = manualJpegQuality(80),
-                sensorSensitivity = highestSensorSensitivity(),
-                frameProcessor = { _ -> }
-            ),
-            logger = loggers(
-                logcat(),
-                fileLogger(this)
-            )
-        )
-    }
-
-    fun rotateBitmap(source: Bitmap, angle: Float): Bitmap {
-        val matrix = Matrix()
-        matrix.postRotate(angle)
-        return Bitmap.createBitmap(source, 0, 0, source.width, source.height, matrix, true)
-    }
-
-    private fun compress(bitmap: Bitmap): String {
-        val byteArrayOutputStream = ByteArrayOutputStream()
-        bitmap.compress(Bitmap.CompressFormat.JPEG, 80, byteArrayOutputStream)
-        val byteArray = byteArrayOutputStream.toByteArray()
-        return Base64.encodeToString(byteArray, Base64.DEFAULT)
-    }
-
-    val requestPermissionLauncher =
-        registerForActivityResult(
-            ActivityResultContracts.RequestPermission()
-        ) { isGranted: Boolean ->
-            if (isGranted) {
-                cameraInit()
-            } else {
-                Toast.makeText(this,"permission deind",Toast.LENGTH_LONG).show()
+                    takePicture(imageCapture)
+                })
+//                toggleFlash.setOnClickListener(View.OnClickListener { setFlashIcon(camera) })
+                preview.setSurfaceProvider(cameraView.surfaceProvider)
+            } catch (e: ExecutionException) {
+                e.printStackTrace()
+            } catch (e: InterruptedException) {
+                e.printStackTrace()
             }
-        }
+        }, ContextCompat.getMainExecutor(this))
+    }
 
-    fun chackandOpencamera(){
+    fun takePicture(imageCapture: ImageCapture?) {
+        val file = File(getExternalFilesDir(null), System.currentTimeMillis().toString() + ".jpg")
+        val outputFileOptions = OutputFileOptions.Builder(file).build()
+        imageCapture!!.takePicture(
+            outputFileOptions,
+            Executors.newCachedThreadPool(),
+            object : ImageCapture.OnImageSavedCallback {
+                override fun onImageSaved(outputFileResults: OutputFileResults) {
+                    runOnUiThread {
+                        Toast.makeText(
+                            this@MainActivity,
+                            "Image saved at: " + file.path,
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                    startCamera(cameraFacing)
+                }
 
+                override fun onError(exception: ImageCaptureException) {
+                    runOnUiThread {
+                        Toast.makeText(
+                            this@MainActivity,
+                            "Failed to save: " + exception.message,
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                    startCamera(cameraFacing)
+                }
+            })
+    }
 
-        if(checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED){
-            cameraInit()
-        }else{
-            requestPermissionLauncher.launch(Manifest.permission.CAMERA)
-        }
+    private fun aspectRatio(width: Int, height: Int): Int {
+        val previewRatio = Math.max(width, height).toDouble() / Math.min(width, height)
+        return if (Math.abs(previewRatio - 4.0 / 3.0) <= Math.abs(previewRatio - 16.0 / 9.0)) {
+            AspectRatio.RATIO_4_3
+        } else AspectRatio.RATIO_16_9
     }
 
     override fun onStart() {
         super.onStart()
-        fotoapparat.start()
     }
 
     override fun onResume() {
-        fotoapparat.start()
         super.onResume()
     }
 
 
     override fun onStop() {
-        fotoapparat.stop()
         super.onStop()
     }
 }
